8615e4e9359865f03a388da15dea5183a220fcdd
Tabish0101 <tabishjeelani93@gmail.com>
2025-12-20 22:50:54 +0500

    feat[Home]: design home component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Home from './app/pages/Home'
 
 function App() {
 
   return (
-    <div className='bg-amber-200'>
-      <h1 className='text-slate-800 text-3xl font-bold'>APIs - TanStack 🚀</h1>
+    <div className='bg-slate-800 h-screen p-2'>
+      {/* <h1 className='text-slate-800 text-3xl font-bold'>APIs - TanStack 🚀</h1> */}
+      <Home/>
     </div>
   )
 }
